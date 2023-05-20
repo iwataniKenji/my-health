f812cf65c0f435b40b5375519b1e3cd5b4573e8a
kenji-iwatani <kleversonkenji@gmail.com>
2023-05-20 13:42:17 -0300

    chore: adds environment variables to project

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-    plugins: ["react-native-reanimated/plugin"],
+    plugins: ["react-native-reanimated/plugin", "module:react-native-dotenv"],
     env: {
       production: {
         plugins: ["react-native-paper/babel"],
---
+++ src/firebase/config.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+// conecta ao módulo de autenticação
+const auth = getAuth(app);
+
+export { auth };
