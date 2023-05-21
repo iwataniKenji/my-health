7b003ec59744273ac16360fe299940aca7e2618c
kenji-iwatani <kleversonkenji@gmail.com>
2023-05-21 11:43:50 -0300

    feat: implements listing filtered by date

--- src/features/next-vaccines/NextVaccinesList.tsx
+++ src/features/next-vaccines/NextVaccinesList.tsx
@@ -1,8 +1,35 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { NextVaccinesCard } from "./NextVaccinesCard";
-import { vaccinesMocked } from "../../data/persistence";
+import { useEffect, useState } from "react";
+import { Vaccine } from "../../types/Vaccine";
+import { db } from "../../firebase/config";
+import { collection, onSnapshot, query } from "firebase/firestore";
 
 export function NextVaccinesList() {
+  // TODO -> mudar para state global
+  const [vaccines, setVaccines] = useState<Vaccine[]>([]);
+
+  const q = query(collection(db, "vaccines"));
+
+  useEffect(() => {
+    onSnapshot(q, (result) => {
+      const vaccinesList: any = [];
+
+      result.forEach((doc: any) => {
+        vaccinesList.push({
+          id: doc.id,
+          title: doc.data().title,
+          doses: doc.data().doses,
+          date: doc.data().date,
+          nextDose: doc.data().nextDose,
+          image: doc.data().image,
+        });
+      });
+
+      setVaccines(vaccines);
+    });
+  }, []);
+
   return (
     <View
       style={{
@@ -13,13 +40,23 @@ export function NextVaccinesList() {
         height: "70%",
       }}
     >
-      {vaccinesMocked.map((item) => (
-        <NextVaccinesCard
-          key={item.id}
-          title={item.title}
-          nextDose={item.nextDose}
-        />
-      ))}
+      <FlatList
+        data={vaccines.filter(({ nextDose }) => {
+          const today = new Date();
+          const nextDoseDay = nextDose ? new Date(nextDose) : new Date();
+
+          return nextDoseDay >= today;
+        })}
+        renderItem={({ item }) => (
+          <NextVaccinesCard
+            key={item.id}
+            title={item.title}
+            nextDose={item.nextDose}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
     </View>
   );
 }
